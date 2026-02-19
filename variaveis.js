c5414b36c371ce2fee55f8ad53bdae4be78f124f
João Pedro <121351231+barbosajuao@users.noreply.github.com>
2026-02-19 18:43:37 -0300

    teste

---
+++ variaveis.js
@@ -0,0 +1,13 @@
+let nome = `joao`
+let idade = 20
+let altura = 1.75
+let peso = 70.5
+let isStudent = true
+
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Altura: ${altura}m`)
+console.log(`Peso: ${peso}kg`)
+console.log(`É estudante? ${isStudent}`)
+ 
+
